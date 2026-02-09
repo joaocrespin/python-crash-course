41d27cb66ed7f269f155f2b656357fe359955d5a
João Guilherme <joaorato58@gmail.com>
2026-02-09 08:42:26 -0300

    Chapter Eighteen Finished

--- Chapter18/pizzeria/pizzas/urls.py
+++ Chapter18/pizzeria/pizzas/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'pizzas'
 urlpatterns = [
     # Home
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('pizzas/', views.pizzas, name='pizzas'),
+    path('pizzas/<int:pizza_id>', views.topping, name='topping'),
 ]
--- Chapter18/pizzeria/pizzas/views.py
+++ Chapter18/pizzeria/pizzas/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Pizza
 
 def index(request):
     '''The home page for pizzeria.'''
     return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+    '''Show all pizzas.'''
+    pizzas = Pizza.objects.order_by('name')
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzas/pizzas.html', context)
+
+def topping(request, pizza_id):
+    '''Show a single pizza and all its toppings.'''
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('name')
+    context = {'pizza':pizza, 'toppings':toppings}
+    return render(request, 'pizzas/topping.html', context)
